75d12d4b99acae43189a41042f590a39ccfeb1b4
NabeelRizwan <150427744+NabeelRizwan@users.noreply.github.com>
2026-04-16 23:10:26 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI
+from database import engine
+import models
 
+from routes import chat, customer, knowledge
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(chat.router, prefix="/chat")
+app.include_router(customer.router, prefix="/customer")
+app.include_router(knowledge.router, prefix="/knowledge")
