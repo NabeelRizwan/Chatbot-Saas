12bffb28fb0f04e0c37be8e29c75a6a1d5c07b46
NabeelRizwan <150427744+NabeelRizwan@users.noreply.github.com>
2026-04-17 22:31:08 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 from database import engine
 import models
@@ -11,6 +12,7 @@ Base.metadata.create_all(bind=engine)
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="backend/static"), name="static")
 
 app.include_router(chat.router, prefix="/chat")
 app.include_router(customer.router, prefix="/customer")
